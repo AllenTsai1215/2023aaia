5028abd75c1371a461eb5c8841dc66cb7533aec4
AllenTsai1215 <allen12151215@gmail.com>
2023-11-07 12:01:13 +0800

    week09

--- week09/week09-7.py
+++ week09/week09-7.py
@@ -3,6 +3,12 @@ class Solution:
         
         a=arr
         N=len(a)
+
+        for k in range(N):
+            for i in range(N-1):
+                if a[i+1]<a[i]:
+                    a[i],a[i+1]=a[i+1],a[i]
+
         for i in range(N-1):
             d=a[1]-a[0]
             if a[i+1]-a[i]!=d:
