756a129afd95ed8f954b46d623cf2911c9375cc8
AllenTsai1215 <allen12151215@gmail.com>
2024-01-02 11:28:46 +0800

    week17

---
+++ week17/week17-2-a.py
@@ -0,0 +1,7 @@
+#SOIT107_ADVANCE_011
+a=input()
+ans=0
+for c in a:
+	if c<='9' and c>='0':
+		ans+=1
+print(ans)
---
+++ week17/week17-4-d.py
@@ -0,0 +1,4 @@
+#SOIT107_Base_017
+a=int(input())
+b=int(input())
+print((a-b*6)//6,(a-b*6)%6,end='')
